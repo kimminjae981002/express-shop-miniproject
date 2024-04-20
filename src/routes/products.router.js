7b1ad31957b8c83351d1b3184ae832259ac75c1b
minjae Kim <cmg981548@gmail.com>
2024-04-20 22:07:36 +0900

    feat: [main] 상품 상세보기

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/products.model');
 const Category = require('../models/categories.model');
-const { getAllCategories } = require('../middlewares/products')
+const fs = require('fs-extra');
+const { getAllCategories } = require('../middlewares/products');
 
 router.get('/', getAllCategories, async (req, res, next) => {
     try {
@@ -21,7 +22,19 @@ router.get('/:category', getAllCategories, async (req, res, next) => {
         const categories = await Product.find({category})
         res.render('products', { products: categories })
     } catch (err) {
-        console.err(err);
+        console.error(err);
+        next(err);
+    }
+})
+
+router.get('/:category/:product', async (req, res, next) => {
+    try {
+        const product = await Product.findOne({ slug: req.params.product });
+        const galleryDir = 'src/public/products-images/' + product._id + '/gallery';
+        const galleryImages = await fs.readdir(galleryDir); // 경로에 해당하는 파일을 읽어온다.
+        res.render('product', { product, galleryImages })
+    } catch (err) {
+        console.error(err);
         next(err);
     }
 })
