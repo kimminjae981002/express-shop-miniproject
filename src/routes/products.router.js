9c4992dac7fb6467e54cdba2f0a269236378a780
minjae Kim <cmg981548@gmail.com>
2024-04-20 21:59:10 +0900

    feat: [main] 카테고리 별 클릭했을 때 카테고리에 해당하는 상품 보여주기

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -7,12 +7,23 @@ const { getAllCategories } = require('../middlewares/products')
 router.get('/', getAllCategories, async (req, res, next) => {
     try {
         const products = await Product.find();
-        res.render('products', {products})
+        res.render('products', { products })
     } catch (err) {
         console.error(err);
         next(err);
     }
     res.render('products')
+});
+
+router.get('/:category', getAllCategories, async (req, res, next) => {
+    try {
+        const { category } = req.params;
+        const categories = await Product.find({category})
+        res.render('products', { products: categories })
+    } catch (err) {
+        console.err(err);
+        next(err);
+    }
 })
 
 
