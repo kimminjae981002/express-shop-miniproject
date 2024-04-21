0fb9a594e4742d5034730cd95885d9bd33cbf4cd
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-04-21 21:55:04 +0900

    feat: [main] 장바구니 업데이트 및 비우기

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/products.model')
+const Product = require('../models/products.model');
+
+// user model cart를 심엇을  거 같다.
 
 // 장바구니에 상품 추가
 router.post('/:product', async (req, res, next) => {
@@ -48,5 +50,57 @@ router.post('/:product', async (req, res, next) => {
         next(err);
     }
 })
+
+router.get('/checkout', async (req, res, next) => {
+    res.render('checkout')
+})
+
+router.get('/update/:product', async (req, res, next) => {
+    try {
+         const slug = req.params.product;
+         let cart = req.session.cart;
+         const operator = req.query.action;
+        for (let i = 0; i < cart.length; i++){
+            if (cart[i].title === slug) {
+                if (operator === 'add') {
+                    cart[i].qty += 1;
+                    req.flash('success', '상품 개수가 추가되었습니다.');
+                } else if (operator === 'remove') {
+                    cart[i].qty -= 1;
+                    if (cart[i].qty <= 0) {
+                        cart.splice(i, 1);
+                    };
+                    req.flash('success', '상품 개수가 제거되었습니다.');
+                } else if (operator === 'clear') {
+                    cart.splice(i, 1);
+                    req.flash('success', '상품이 제거되었습니다.');
+                }
+            }
+            break;
+        }
+        res.redirect('/cart/checkout'); // 변경된 카트를 다시 표시하기 위해 리디렉션합니다.
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+})
+
+router.delete('/', async (req, res, next) => {
+    let cart = req.session.cart;
+    try {
+        if (cart.length > 0) {
+            req.session.cart = [];
+            req.flash('success','장바구니가 비어졌습니다.')
+        } else {
+            req.flash('error','장바구니가 비어있습니다.')
+        }
+
+        res.redirect('/cart/checkout')
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+})
+
 
 module.exports = router
